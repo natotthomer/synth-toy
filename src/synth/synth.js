d75c96c3376d65d8af4a2e3a8c66b12236264e79
Nat Homer <nathaniel.ott.homer@gmail.com>
2017-08-05 23:09:31 -0400

    hook up initial audio nodes

---
+++ src/synth/synth.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default class Synth extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  componentWillMount () {
+    this.ac = new (window.AudioContext || window.webkitAudioContext)();
+    console.log(this.ac);
+    this.osc = this.ac.createOscillator();
+    this.gn = this.ac.createGain();
+    this.osc.type = 'triangle';
+    this.osc.start();
+    // this.osc.frequency.value = 280;
+    this.lpf = this.ac.createBiquadFilter();
+    this.lpf.frequency.value = 550
+    // this.osc.connect(this.lpf);
+    this.lpf.connect(this.gn);
+    this.gn.connect(this.ac.destination);
+  }
+
+  render () {
+    return (
+      <div>hi</div>
+    );
+  }
+}
